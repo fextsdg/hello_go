1c98bcabb820df63cc37df47223bd9370dd2d4d2
DELL-ws <2583426565@qq.com>
2025-02-05 16:54:09 +0800

    update channel.go

--- channel/channel.go
+++ channel/channel.go
@@ -14,10 +14,15 @@ func main() {
 	num := 20
 	intchan <- num //赋值
 	intchan <- 2
-	fmt.Printf("intchan:%v,the length of intchan is %v\n", intchan, len(intchan))
+	fmt.Printf("intchan:%v,the length of intchan is %v,the captity of intchan is %v\n", intchan, len(intchan), cap(intchan))
 
 	num1 := <-intchan //取值
 	fmt.Println(num1)
+	fmt.Println(<-intchan)
+	fmt.Printf("intchan:%v,the length of intchan is %v\n", intchan, len(intchan))
+
+	//关闭管道--关闭后不能写入但能读出数据
+	close(intchan)
 	fmt.Println(<-intchan)
 	fmt.Printf("intchan:%v,the length of intchan is %v\n", intchan, len(intchan))
 
